cfcc7223ed45cf3e0d15953324cf2a0cf55b33d7
ShizukuIchi <shizukuichi@gmail.com>
2019-01-13 16:55:05 +0800

    syntax

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
       y: Math.floor(Math.random() * 8) + 8,
     },
     // player: { x: 0, y: 0 },
-    // monster: { x: 8, y: 0 },
+    // monster: { x: 0, y: 5 },
     items: [],
     key: '',
     score: 0,
@@ -28,71 +28,67 @@ class App extends Component {
   changeKey = ({ key }) => {
     if (key === 'Enter') {
       this.action();
-    } else if ('wasdef'.includes(key)) this.setState({ key });
+      this.setState({ key: '' });
+    } else if ('wsadefq'.includes(key)) this.setState({ key });
   };
   action() {
-    const pos = { ...this.state.player };
+    this.monsterAttack();
     switch (this.state.key) {
-      case 'w':
-        if (checkWall(pos, 0)) {
-          pos.y -= 1;
-        } else {
-          alert('Boom!');
-        }
-        break;
-      case 's':
-        if (checkWall(pos, 1)) {
-          pos.y += 1;
-        } else {
-          alert('Boom!');
-        }
-        break;
-      case 'a':
-        if (checkWall(pos, 2)) {
-          pos.x -= 1;
-        } else {
-          alert('Boom!');
-        }
-        break;
-      case 'd':
-        if (checkWall(pos, 3)) {
-          pos.x += 1;
-        } else {
-          alert('Boom!');
-        }
-        break;
       case 'e':
-        return this.pick();
+        this.pick();
+        break;
       case 'f':
-        return this.attack();
+        this.attack();
+        break;
+      case 'w':
+      case 's':
+      case 'a':
+      case 'd':
+        this.movePlayer();
+        break;
       default:
     }
-    const monster = this.dfs(this.state.monster, 0, 42, pos);
-    let hp = this.state.hp;
-    if (
-      this.state.monster.x === this.state.player.x &&
-      this.state.monster.y === this.state.player.y
-    ) {
-      if (hp > 2) {
-        hp -= 2;
-      } else {
-        this.setState({
-          player: pos,
-          key: '',
-          hp: 0,
-          monster: monster ? monster : this.state.monster,
-          status: 'gameOver',
-        });
-        return;
-      }
+    this.moveMonster();
+  }
+  monsterAttack() {
+    const { hp, monster, player, key } = this.state;
+    if (monster.x !== player.x || monster.y !== player.y || key === 'f') return;
+    if (hp > 2) {
+      this.setState({
+        hp: hp - 2,
+      });
+    } else {
+      this.setState({
+        hp: 0,
+        status: 'gameOver',
+      });
     }
-
-    this.setState({
-      player: pos,
-      key: '',
-      hp,
-      monster: monster ? monster : this.state.monster,
-    });
+  }
+  moveMonster() {
+    const monster = dfs(this.state.monster, 0, 42, this.state.player);
+    if (monster) this.setState({ monster });
+  }
+  movePlayer() {
+    const { player, key } = this.state;
+    const pos = { ...player };
+    const dir = 'wsad'.indexOf(key);
+    if (!checkWall(pos, dir)) return alert('Boom!');
+    switch (dir) {
+      case 0:
+        pos.y -= 1;
+        break;
+      case 1:
+        pos.y += 1;
+        break;
+      case 2:
+        pos.x -= 1;
+        break;
+      case 3:
+        pos.x += 1;
+        break;
+      default:
+    }
+    this.setState({ player: pos });
   }
   attack() {
     if (
@@ -151,35 +147,7 @@ class App extends Component {
     if (item) c = 'G';
     return <Item c={c} {...this.state.player} />;
   }
-  dfs = (pos, depth, last, playerPos) => {
-    if (depth === 5) return false;
-    if (playerPos.x === pos.x && playerPos.y === pos.y) return pos;
-    for (let dir = 0; dir < 4; dir++) {
-      if ((last ^ 1) === dir) continue;
-      if (checkWall(pos, dir)) {
-        let newPos = { ...pos };
-        switch (dir) {
-          case 0:
-            newPos.y -= 1;
-            break;
-          case 1:
-            newPos.y += 1;
-            break;
-          case 2:
-            newPos.x -= 1;
-            break;
-          case 3:
-            newPos.x += 1;
-            break;
-          default:
-        }
-        if (this.dfs(newPos, depth + 1, dir, playerPos)) {
-          return newPos;
-        }
-      }
-    }
-    return false;
-  };
+
   render() {
     if (this.state.status === 'gameOver') {
       window.removeEventListener('keypress', this.changeKey);
@@ -218,4 +186,34 @@ function checkWall(pos, dir) {
     return false;
   }
 }
+function dfs(pos, depth, last, playerPos) {
+  if (depth === 5) return false;
+  if (playerPos.x === pos.x && playerPos.y === pos.y) return pos;
+  for (let dir = 0; dir < 4; dir++) {
+    if ((last ^ 1) === dir) continue;
+    if (checkWall(pos, dir)) {
+      let newPos = { ...pos };
+      switch (dir) {
+        case 0:
+          newPos.y -= 1;
+          break;
+        case 1:
+          newPos.y += 1;
+          break;
+        case 2:
+          newPos.x -= 1;
+          break;
+        case 3:
+          newPos.x += 1;
+          break;
+        default:
+      }
+      if (dfs(newPos, depth + 1, dir, playerPos)) {
+        return newPos;
+      }
+    }
+  }
+  return false;
+}
+
 export default App;
